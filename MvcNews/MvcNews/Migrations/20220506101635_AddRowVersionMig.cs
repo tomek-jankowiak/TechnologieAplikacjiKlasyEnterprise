754d7cf70c469d7b54652902b48f24c2b786f99e
tomek <tomek.jankowiak01@gmail.com>
2022-05-19 19:31:46 +0200

    08. ASP.NET MVC

---
+++ MvcNews/MvcNews/Migrations/20220506101635_AddRowVersionMig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcNews.Migrations
+{
+    public partial class AddRowVersionMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(140)",
+                maxLength: 140,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "News",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "News");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "News",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(140)",
+                oldMaxLength: 140);
+        }
+    }
+}
